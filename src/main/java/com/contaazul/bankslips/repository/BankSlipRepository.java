29ffd7f0436a3ec2e258f7b74b573806f6248d05
Paulo Correa <paulocorrea.ads@gmail.com>
2018-05-04 15:17:31 -0400

    implementada obtencao de boleto sem detalhes

--- src/main/java/com/contaazul/bankslips/repository/BankSlipRepository.java
+++ src/main/java/com/contaazul/bankslips/repository/BankSlipRepository.java
@@ -1,8 +1,10 @@
 package com.contaazul.bankslips.repository;
 
 import com.contaazul.bankslips.model.BankSlip;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -12,4 +14,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BankSlipRepository extends JpaRepository<BankSlip, UUID>{
     
+    @Query("select new com.contaazul.bankslips.model.BankSlip(b.id, b.dueDate, b.totalInCents, b.customer) from BankSlip b")
+    List<BankSlip> getAllWithoutDetails();
 }
