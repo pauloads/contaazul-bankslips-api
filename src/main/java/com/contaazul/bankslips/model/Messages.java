0093654f922abbbebe8b5f792dab4999f7965998
Paulo Correa <paulocorrea.ads@gmail.com>
2018-05-04 15:20:21 -0400

    adicionado enum de mensagens de retorno

---
+++ src/main/java/com/contaazul/bankslips/model/Messages.java
@@ -0,0 +1,27 @@
+package com.contaazul.bankslips.model;
+
+/**
+ *
+ * @author Paulo
+ */
+public enum Messages {
+
+    BANKSLIP_CREATED("Bankslip created"),
+    BANKSLIP_NOT_PROVIDED("Bankslip not provided in the request body"),
+    INVALID_BANKSLIP_PROVIDED("Invalid bankslip provided.The possible reasons are: A field of the provided bankslip was null or with invalid values"),
+    INVALID_ID_PROVIDED("Invalid id provided - it must be a valid UUID"),
+    BANKSLIP_NOT_FOUND("Bankslip not found with the specified id"),
+    BANKSLIP_PAID("Bankslip paid"),
+    BANKSLIP_CANCELED("Bankslip canceled");
+
+    private final String message;
+
+    private Messages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
