c3782bafb796323aca601f429162268aa69feafe
Paulo Correa <paulocorrea.ads@gmail.com>
2018-05-04 15:13:31 -0400

    implementados endpoints restantes

--- src/main/java/com/contaazul/bankslips/controller/BankSlipController.java
+++ src/main/java/com/contaazul/bankslips/controller/BankSlipController.java
@@ -1,11 +1,21 @@
 package com.contaazul.bankslips.controller;
 
 import com.contaazul.bankslips.model.BankSlip;
+import com.contaazul.bankslips.model.Messages;
+import static com.contaazul.bankslips.model.Messages.BANKSLIP_CANCELED;
+import static com.contaazul.bankslips.model.Messages.BANKSLIP_PAID;
 import com.contaazul.bankslips.service.BankSlipService;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.OK;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,14 +28,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rest/bankslips")
 public class BankSlipController {
-    
+
     @Autowired
     private BankSlipService service;
-    
+
     @PostMapping
     @ResponseBody
-    public ResponseEntity<?> create(@RequestBody BankSlip bankSlip){
+    public ResponseEntity<?> create(@RequestBody BankSlip bankSlip) {
         service.create(bankSlip);
-        return new ResponseEntity("Bankslip created", HttpStatus.CREATED);
+        return new ResponseEntity(Messages.BANKSLIP_CREATED.getMessage(), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<List<BankSlip>> list() {
+        List<BankSlip> allBankslips = service.listAll();
+        return new ResponseEntity<>(allBankslips, OK);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<BankSlip> getDetails(@PathVariable("id") UUID id) {
+        BankSlip detailedBankSlip = service.getDetails(id);
+        return new ResponseEntity<>(detailedBankSlip, OK);
+    }
+
+    @PutMapping("/{id}/pay")
+    @ResponseBody
+    public ResponseEntity<String> pay(@PathVariable("id") UUID id) {
+        service.pay(id);
+        return new ResponseEntity<>(BANKSLIP_PAID.getMessage(), OK);
+    }
+
+    @DeleteMapping("/{id}/cancel")
+    @ResponseBody
+    public ResponseEntity<String> cancel(@PathVariable("id") UUID id) {
+        service.cancel(id);
+        return new ResponseEntity<>(BANKSLIP_CANCELED.getMessage(), OK);
     }
 }
