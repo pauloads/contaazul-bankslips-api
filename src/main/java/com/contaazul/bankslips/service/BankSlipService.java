943345b5e5e878089582644b21ba91b733a96667
Paulo Correa <paulocorrea.ads@gmail.com>
2018-05-04 15:19:51 -0400

    implementados metodos de cancelamento, listagem, pagamento e detalhamento de boletos

--- src/main/java/com/contaazul/bankslips/service/BankSlipService.java
+++ src/main/java/com/contaazul/bankslips/service/BankSlipService.java
@@ -1,7 +1,12 @@
 package com.contaazul.bankslips.service;
 
 import com.contaazul.bankslips.model.BankSlip;
+import com.contaazul.bankslips.model.BankSlipStatus;
+import static com.contaazul.bankslips.model.BankSlipStatus.CANCELED;
+import static com.contaazul.bankslips.model.BankSlipStatus.PAID;
 import com.contaazul.bankslips.repository.BankSlipRepository;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +16,34 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BankSlipService {
-    
+
     @Autowired
     private BankSlipRepository repository;
-    
-    public void create(BankSlip bankSlip){
+
+    public void create(BankSlip bankSlip) {
         repository.save(bankSlip);
     }
+
+    public List<BankSlip> listAll() {
+        return repository.getAllWithoutDetails();
+    }
     
+    public BankSlip getDetails(UUID id){
+        BankSlip detailedBankslip = repository.findById(id).get();
+        detailedBankslip.calculateFine();
+        return detailedBankslip;
+    }
+
+    public void pay(UUID id) {
+        BankSlip paidBankslip = repository.findById(id).get();
+        paidBankslip.setStatus(PAID);
+        repository.save(paidBankslip);
+    }
+
+    public void cancel(UUID id) {
+        BankSlip canceledbankslip = repository.findById(id).get();
+        canceledbankslip.setStatus(CANCELED);
+        repository.save(canceledbankslip);
+    }
+
 }
